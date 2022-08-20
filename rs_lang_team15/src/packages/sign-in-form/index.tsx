78ebb2e638ae361094328a389ce1ab6773d8ffd9
Pavel.V <verbipo@gmail.com>
2022-08-20 10:25:05 +0600

    feat: Sign-In form

---
+++ rs_lang_team15/src/packages/sign-in-form/index.tsx
@@ -0,0 +1,85 @@
+// https://mui.com/material-ui/react-text-field/#type-quot-number-quot
+import FormControl from "@mui/material/FormControl";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import "./style.css";
+import React from "react";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+interface State {
+  password: string;
+  showPassword: boolean;
+}
+
+export default function SignInForm() {
+  const [values, setValues] = React.useState<State>({
+    password: "",
+    showPassword: false,
+  });
+
+  const handleChange =
+    (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="sign-in-container">
+      <Typography variant="h3">
+        <b>Login!</b>
+      </Typography>
+
+      <TextField
+        sx={{ m: 1, width: "25ch" }}
+        name="email"
+        type="email"
+        placeholder="example@email.com"
+        label="Email"
+      />
+
+      <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={values.showPassword ? "text" : "password"}
+          value={values.password}
+          onChange={handleChange("password")}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
+
+      <Button variant="contained">Log in</Button>
+      <Button href="#text-buttons">Don't have an account? Sign Up</Button>
+    </div>
+  );
+}
