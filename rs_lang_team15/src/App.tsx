92079036d23a19d30b1ca93fa1ca8010ce387281
Pavel.V <verbipo@gmail.com>
2022-08-20 18:55:58 +0600

    feat: about mudule

--- rs_lang_team15/src/App.tsx
+++ rs_lang_team15/src/App.tsx
@@ -3,12 +3,14 @@ import SignInForm from "./packages/sign-in-form";
 import SignUpForm from "./packages/sign-up-form";
 import Footer from "./packages/footer";
 import AdvatagesBlock from "./packages/advantages-block";
+import TeamBlock from "./packages/about-team";
 
 function App() {
   return (
     <>
     <Header />
     <AdvatagesBlock />
+    <TeamBlock />
     <Footer />
     <SignInForm />
     <SignUpForm />
---
+++ rs_lang_team15/src/packages/about-team/index.tsx
@@ -0,0 +1,41 @@
+import "./style.css";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import TeamList from "./team-list";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+
+export default function TeamBlock() {
+  return (
+    <div className="team-container">
+      {TeamList.map((el, index) => (
+        <Paper elevation={6}>
+          <Card sx={{ maxWidth: 345 }} data-index={index}>
+            <CardMedia
+              component="img"
+              height="350"
+              image={el.photo}
+              alt="avatar"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h4" component="div">
+                {el.name}
+              </Typography>
+              <Typography gutterBottom variant="subtitle1" component="div">
+                {el.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {el.contribution}
+              </Typography>
+              <Link href={el.github} underline="none">
+                {el.githubName}
+              </Link>
+            </CardContent>
+          </Card>
+        </Paper>
+      ))}
+    </div>
+  );
+}
