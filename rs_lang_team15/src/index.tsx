5c416d56008ee2901f49d56daba14cd307e2e2a2
Pavel.V <verbipo@gmail.com>
2022-08-20 17:52:52 +0600

    fix: modules structure rework

--- rs_lang_team15/src/index.tsx
+++ rs_lang_team15/src/index.tsx
@@ -1,27 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Header from "./packages/header";
-import SignInForm from "./packages/sign-in-form";
-import SignUpForm from "./packages/sign-up-form";
-import Footer from "./packages/footer";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Header />
-    <SignInForm />
-    <SignUpForm />
-    <Footer />
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
