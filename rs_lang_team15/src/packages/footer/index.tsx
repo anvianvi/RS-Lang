4c64ed6fdb8a4e258fa864f7bc3bd54b5209721f
Pavel.V <verbipo@gmail.com>
2022-08-22 18:01:46 +0600

    fix: year

--- rs_lang_team15/src/packages/footer/index.tsx
+++ rs_lang_team15/src/packages/footer/index.tsx
@@ -24,7 +24,7 @@ export default function Footer() {
           Pavel
         </Link>
       </div>
-      <Typography>© 2021</Typography>
+      <Typography>© 2022</Typography>
     </div>
   );
 }
