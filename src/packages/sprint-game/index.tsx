04dca71f6d8d921b213eddd307db1150683f71b9
Pavel.V <verbipo@gmail.com>
2022-08-30 20:57:25 +0600

    feat: gameScreensLogic

--- src/packages/sprint-game/index.tsx
+++ src/packages/sprint-game/index.tsx
@@ -21,72 +21,102 @@ const renderTime = ({ remainingTime }: RemainingTime) => {
   );
 };
 
-function CountDown() {
-  return (
-    <div className="timer-wrapper">
-      <CountdownCircleTimer
-        size={150}
-        isPlaying
-        duration={45}
-        colors={["#1976d2", "#1976d2", "#1976d2", "#1976d2"]}
-        colorsTime={[10, 6, 3, 0]}
-        onComplete={() => ({ shouldRepeat: false, delay: 1 })}
+let SCORE: number = 0;
+let GAMEDURATION: number = 20000;
+
+function GameScreans() {
+  const [GaemeFase, setGameFase] = useState(0);
+  const [WordsPased, setwordsPased] = useState(0);
+
+  function setGameTime() {
+    setGameFase(GaemeFase + 2);
+  }
+
+  if (GaemeFase === 0) {
+    return (
+      <Button
+        variant="contained"
+        onClick={() => {
+          setGameFase(GaemeFase + 1);
+          setTimeout(setGameTime, GAMEDURATION);
+        }}
       >
-        {renderTime}
-      </CountdownCircleTimer>
-    </div>
-  );
-}
-
-function GameField() {
-  const [count, setCount] = useState(1);
-
-  return (
-    <>
-      <CountDown />
-      <Typography variant="h5">{TextBookWordsList[count].word}</Typography>
-      <Typography variant="subtitle1">
-        {TextBookWordsList[count].wordTranslate}
-      </Typography>
-      <div className="game-field-buttons-container">
-        <Button
-          size="large"
-          variant="outlined"
-          onClick={() =>
-            count < TextBookWordsList.length - 1
-              ? setCount(count + 1)
-              : setCount(count)
-          }
+        Start
+      </Button>
+    );
+  }
+  if (GaemeFase === 1) {
+    return (
+      <>
+        <CountdownCircleTimer
+          size={150}
+          isPlaying
+          duration={GAMEDURATION/1000}
+          colors={["#1976d2", "#1976d2", "#1976d2", "#1976d2"]}
+          colorsTime={[10, 6, 3, 0]}
+          onComplete={() => ({ shouldRepeat: false, delay: 1 })}
         >
-          true
-        </Button>
+          {renderTime}
+        </CountdownCircleTimer>
+        <Typography variant="h5">
+          {TextBookWordsList[WordsPased].word}
+        </Typography>
+        <Typography variant="subtitle1">
+          {TextBookWordsList[WordsPased].wordTranslate}
+        </Typography>
+        <div className="game-field-buttons-container">
+          <Button
+            size="large"
+            variant="outlined"
+            onClick={() => {
+              WordsPased < TextBookWordsList.length - 1
+                ? setwordsPased(WordsPased + 1)
+                : setwordsPased(WordsPased);
+              SCORE++;
+            }}
+          >
+            true
+          </Button>
+          <Button
+            size="large"
+            variant="outlined"
+            color="error"
+            onClick={() =>
+              WordsPased > 1
+                ? setwordsPased(WordsPased - 1)
+                : setwordsPased(WordsPased)
+            }
+          >
+            false
+          </Button>
+        </div>
+        <p>You clicked {WordsPased} times</p>
+      </>
+    );
+  }
+  if (GaemeFase === 2) {
+    return (
+      <>
+        <p>Congratulates you score : {SCORE}</p>
         <Button
-          size="large"
-          variant="outlined"
-          color="error"
-          onClick={() => (count > 1 ? setCount(count - 1) : setCount(count))}
+          variant="contained"
+          onClick={() => {
+            setGameFase(GaemeFase - 2);
+          }}
         >
-          false
+          {" "}
+          restart{" "}
         </Button>
-      </div>
-      <p>You clicked {count} times</p>
-    </>
-  );
+      </>
+    );
+  }
+  return <p>Somth go wrong pls reloude the page (press F5)</p>;
 }
 
 export default function SprintGame() {
   return (
     <div className="game-container" id="game-container">
-      {/* <Button
-        variant="contained"
-        onClick={() => {
-          const field = document.getElementById("game-container");
-          ReactDOM.render(<GameField />, field);
-        }}
-      >
-        Start
-      </Button> */}
-      <GameField />
+      <GameScreans />
     </div>
   );
 }
