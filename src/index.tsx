ea24c75c0fde2ce125159e801a743cec353bf460
Pavel.V <verbipo@gmail.com>
2022-09-03 20:08:16 +0600

    feat: ReactRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,20 @@ import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { BrowserRouter } from "react-router-dom";
+const { PUBLIC_URL } = process.env;
+console.log("PUBLIC_URL", PUBLIC_URL);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-       <GoogleOAuthProvider
-    clientId='1041551134370-uaa0knavh9lmn0sc8cgev975hg29f9fj.apps.googleusercontent.com'
-    >
-    <App />
-    </GoogleOAuthProvider>
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId="1041551134370-uaa0knavh9lmn0sc8cgev975hg29f9fj.apps.googleusercontent.com">
+        <App />
+      </GoogleOAuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
