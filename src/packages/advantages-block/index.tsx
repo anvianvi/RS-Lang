6ec44ea1a469c3eb94baf4455415e9f7f1c25c69
Dmitry <p.dmitry.y@gmail.com>
2022-09-05 23:45:48 +0300

    feat: add information about app

--- src/packages/advantages-block/index.tsx
+++ src/packages/advantages-block/index.tsx
@@ -9,6 +9,17 @@ import Paper from "@mui/material/Paper";
 export default function AdvatagesBlock() {
   return (
     <div className="advantage-block-container">
+      <div className="about app">
+        <p>
+          Our app is designed to help you learn English and increase your vocabulary. 
+          In the Textbook tab you can find 3600 frequently used English words with translation, transcription, usage examples and audio. 
+          All words are divided into 6 sections of 30 pages. Playing games will help you consolidate the learned words and keep them in your memory.
+        </p>
+        <p>
+          We tried very hard to make the best app for you but unfortunately, due to the circumstances, the application is not finished yet and needs to be improved.
+          It would be great if you give us at least a couple more days to finalize app.
+        </p>
+      </div>
       {AdvantagesList.map((el, index) => (
           <Paper elevation={5} data-indexd={index} key={el.title + "id"}>
             <Card sx={{ maxWidth: 330 }} className="card-container">
